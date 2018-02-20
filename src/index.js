926ea5e8fb7ac1677fd5d27520c389e5bf219d3a
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-20 22:30:58 +0300

    all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let split = equation.split(' ');
+  let a = Number(split[0]);
+  let b = Number(split[3].concat(split[4]));
+  let c = Number(split[7].concat(split[8]));
+
+  let discr = b * b - 4 * a * c;
+  let firstRoot = Math.round((-1 * b + Math.sqrt(discr)) / (2 * a));
+  let secondRoot = Math.round((-1 * b - Math.sqrt(discr)) / (2 * a));
+  return [firstRoot, secondRoot].sort((a, b) => a - b);
+};
